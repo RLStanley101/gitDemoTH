a30fb1ef67b61c8a61b07fb3eda35401fbbdf15c
RLStanley101 <rlstanley101@gmail.com>
2016-09-11 13:02:35 -0400

    Update ViewController.swift
    username changed

--- gitDemoTH/ViewController.swift
+++ gitDemoTH/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let userName = "dummyuser"
+    let userName = "dummyuserchanged"
 
     override func viewDidLoad() {
         super.viewDidLoad()
